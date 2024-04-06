eb2a2ebb54d4b9b6cc28a9308f4fbeed58ff39d6
Donimika <141586347+Donimika@users.noreply.github.com>
2024-04-06 14:32:21 +0200

    DONE: configuration, pom.xml, AppMain

---
+++ src/main/java/taskmanager/AppMain.java
@@ -0,0 +1,11 @@
+package taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(taskmanager.AppMain.class, args);
+    }
+}
